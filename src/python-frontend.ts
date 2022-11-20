b877bfd64bb1b0f206d9e45b10fb464a9edaf28c
William Stein <wstein@sagemath.com>
2022-11-19 19:55:03 -0800

    add some more python usage

--- src/python-frontend.ts
+++ src/python-frontend.ts
@@ -2,15 +2,23 @@ export default async function python() {
   const element = document.createElement("pre");
   document.body.appendChild(element);
   console.log("This is python...!");
-  await window.electronAPI.pythonExec("import sys");
-  const t0 = performance.now();
+  let t0 = performance.now();
+  await window.electronAPI.pythonExec("import os, sys");
+  element.innerText = `Python startup time: ${performance.now() - t0}ms\n`;
+  t0 = performance.now();
   const v = await window.electronAPI.pythonRepr("sys.version");
   console.log(v);
-  element.innerText =
-    v.slice(1, -1) + `\nTime: ${performance.now() - t0}ms\n`;
+  element.innerText +=
+    "\n" + v.slice(1, -1) + `\nTime: ${performance.now() - t0}ms\n`;
+
+  // filesystem
+  element.innerText +=
+    "\nNative Files:\n" +
+    (await window.electronAPI.pythonRepr("' '.join(os.listdir('/'))")) +
+    "\n";
 
   await window.electronAPI.pythonExec(`
-import pandas, json, os, sys
+import pandas
 from contextlib import redirect_stdout
 import io
 f = io.StringIO()
@@ -24,8 +32,8 @@ s = f.getvalue()
   console.log(pv);
   pv = pv.split("\\n").join("\n").slice(1, -1);
   console.log(pv);
-  pv = `Time to import pandas and get versions: ${
+  pv = `Time to import pandas and get versions: ${Math.round(
     performance.now() - t0
-  }ms\n${pv}`;
+  )}ms\n${pv}`;
   element.innerText += "\n" + pv;
 }
