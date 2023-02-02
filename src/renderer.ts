1c9b49ca366e88744f2c40f6ce8397eea1328788
William Stein <wstein@sagemath.com>
2023-02-01 17:40:01 -0800

    deprecate/move and relicense

--- src/renderer.ts
+++
@@ -1,8 +0,0 @@
-//import python from "./python-frontend";
-//python();
-
-import pythonTerminal from "./python-terminal";
-
-const element = document.createElement("div");
-document.body.appendChild(element);
-pythonTerminal(element);
